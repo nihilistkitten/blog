4ac79f943b7c615d1a23b098905b4d3599d5edc4
Riley Shahar <riley.shahar@gmail.com>
2021-03-03 16:03:30 -0800

    Add a toggleable monospace font
    Resolves #1.

--- public/js/preferences.js
+++ public/js/preferences.js
@@ -2,7 +2,58 @@ const DARKMODE_COOKIE_NAME = "dark-theme";
 const DARKMODE_CLASS = "dark-theme";
 const LIGHTMODE_CLASS = "light-theme";
 
+const FONT_COOKIE_NAME = "monospace-font";
+const MONOSPACE_FONT_FAMILY =
+    '"PT Mono", Monaco, "Lucida Console", Courier, monospace';
+const PROPORTIONAL_FONT_FAMILY = '"PT Sans", Helvetica, Arial, sans-serif';
+
 window.onload = () => {
+  setColorschemeOnload();
+  setFontOnload();
+};
+
+function setFontOnload() {
+  var monospace = check_cached_font_preference();
+
+  if (monospace == null) {
+    monospace = false;
+  }
+
+  setMonospaceFont(monospace);
+  if (monospace) {
+    if ((slider = document.getElementById("font-slider")) != null) {
+      slider.checked = true;
+    }
+  }
+}
+
+/// check the preferred font from localstorage
+function check_cached_font_preference() {
+  switch (localStorage.getItem(FONT_COOKIE_NAME)) {
+  case "true":
+    return true;
+  case "false":
+    return false;
+  default:
+    return null;
+  }
+}
+
+function setMonospaceFont(monospace) {
+  if (monospace) {
+    document.body.style.fontFamily = MONOSPACE_FONT_FAMILY;
+  } else {
+    document.body.style.fontFamily = PROPORTIONAL_FONT_FAMILY;
+  }
+}
+
+function togglePreferredFont() {
+  var monospace = document.body.style.fontFamily == MONOSPACE_FONT_FAMILY;
+  setMonospaceFont(!monospace);
+  localStorage.setItem(FONT_COOKIE_NAME, !monospace);
+}
+
+function setColorschemeOnload() {
   var darkmode = check_cached_colorscheme_preference();
 
   if (darkmode == null) {
@@ -15,7 +66,7 @@ window.onload = () => {
       slider.checked = true;
     }
   }
-};
+}
 
 /// check the colorscheme from localstorage
 function check_cached_colorscheme_preference() {

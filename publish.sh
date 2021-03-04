9b41722ea543ed3e46eae6d68ead73d34065f17f
Riley Shahar <riley.shahar@gmail.com>
2021-03-04 04:07:07 -0800

    Set the frontmatter in the publish script
    Progress on #2.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+
 today=$(date +%Y-%m-%d)
-mv "_posts/$1" "_posts/$today-$1"
+new_filename="_posts/$today-$1"
+mv "_posts/$1" $new_filename
+
+echo "Enter a title: "
+read title
+
+echo "Enter a space-separated list of tags: "
+read tags
+
+FRONTMATTER="---
+layout: post
+title: ${title}
+tags: ${tags}
+---"
+
+echo -e "${FRONTMATTER}\n$(cat $new_filename)" > $new_filename
